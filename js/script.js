0c47a13c165dc69293548e0cf7aa72fba0401567
Holmfred <fredrik1304@live.no>
2021-10-07 20:51:35 +0200

    Fin

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
-const url = "https://www.fredrikolsen.one/wp-json/wp/v2/posts?_embed";
-const productContainer = document.querySelector(".latest-blogs");
+const url = "https://www.fredrikolsen.one/wp-json/wp/v2/posts";
+const productContainer = document.querySelector(".latest-games");
 
-async function getGames(){
+async function getAllGames(){
 
     try {
         const response = await fetch(url);
         const results = await response.json();
         console.log(results)
-        
-        for (let i = 0; i < results.length ;i++){
-            productContainer.innerHTML += 
-            `
-            <div class="latest-games">
-                <div>
-                    <img src="${results[i]._embedded['wp:featuredmedia']['0'].source_url}" alt ="${results[i].title.rendered}">
-                    <a href="details.html?id=${results[i].id}"><button class="read-more">Read More</button></a>
-                </div>
-                <div>
-                    <h2>${results[i].title.rendered}</h2>
-                    <div class="paragraphs">
-                        <p>${results[i].excerpt.rendered}</p>
-                    </div>
-                </div>
-            </div>
-            `
-
-            if (i === 3) {
-                break
-            }
-        }
-
+        createHTML(results)
     }
+
     catch(error){
         console.log(error);
     }
 }
 
-getGames()
+getAllGames()
+
+function createHTML(results){
+    productContainer.innerHTML += ""
+
+    for (let i = 0; i < results.length; i++){
+            
+        if(i===2){
+            break
+        }  
+
+        productContainer.innerHTML += 
+        `
+        <div>
+            <img src="${results[i].featured_media_src_url}" alt ="${results[i].title.rendered}">
+            <a href="details.html?id=${results[i].id}"><button class="read-more">Read More</button></a>
+        </div>
+        <div>
+            <h2>${results[i].title.rendered}</h2>
+            <div class="paragraphs">
+                <p>${results[i].excerpt.rendered}</p>
+            </div>
+        </div>
+        ` 
+    }
+}
